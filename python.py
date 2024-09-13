51b9daaf357e6324a81c7d979feff738e47bc246
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-09-13 23:52:57 +0900

    2nd lab session completed.

--- python.py
+++ python.py
@@ -1,2 +1,17 @@
-import math
-print(math.exp(3, 2))
+import random
+
+x = random.randint(1, 10)
+
+print("Welcome to the number guess game!")
+print("Enter number between 1 and 10")
+guessedNumber = input("Your number: ")
+
+if x == int(guessedNumber):
+    print(x)
+    print("Congratulations! You guessed it.")
+elif x > int(guessedNumber):
+    print(x)
+    print("GAME OVER! Too low.")
+elif x < int(guessedNumber):
+    print(x)
+    print("GAME OVER! Too high.")
