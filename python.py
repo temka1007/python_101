ce5d617d82baca8dcb25eacb28224c5449c84db0
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-10-15 18:45:46 +0900

    Check if a string is a palindrome

--- python.py
+++ python.py
@@ -1,27 +1,9 @@
-import random
+inputWord = input("enter your word: ", )
 
-randomNumber = random.randint(1, 100)
-print(randomNumber)
+word = inputWord.replace(" ", "").lower()
 
-guessedNumbers = []
+print(word)
 
-isGameEnded = True
-
-def game(value):
-    number = int(input("Guess a number between 1 and 10: ", ))
-    if value == number:
-        global isGameEnded
-        isGameEnded = False
-        guessedNumbers.append(number)
-        print(guessedNumbers)
-        print(f"Correct! You found the number in {len(guessedNumbers)} guesses.")
-    elif number < value:
-        guessedNumbers.append(number)
-        print("Too low!")
-    elif number > value:
-        guessedNumbers.append(number)
-        print("Too high!")
-
-while isGameEnded:
-    game(randomNumber)
-
+reversedWord = word[::-1]
+# List[start:end:step]
+print(word == reversedWord)
