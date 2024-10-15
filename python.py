f55954092e73e0e0f988bad70dc7cfec3673c49f
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-10-15 19:21:23 +0900

    Prime number checker

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
-#inputWord = input("enter your word: ", )
+isPrime = int(input("Enter your number to check if it is prime number or not: ", ))
 
-string = input("Enter your f in word: ", )
-
-lst = string.split()
-
-print(len(lst))
+for number in range(2, isPrime): 
+    value = isPrime/number
+    if value.is_integer():
+        print("Not prime number!!!")
+        break
+    elif isPrime == number+1:
+        print("Prime number!!!")
+        
+        
