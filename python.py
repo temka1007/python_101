b09861b8b7ffb8fd2bd439f361eaaf8d7ad69f01
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-10-16 14:00:47 +0900

    Find the missing number

--- python.py
+++ python.py
@@ -1,7 +1,12 @@
 lengthOfList = int(input("how many number supposed to be in here?: ", ))
 lst = eval(input("Give your list as follows [a, b, c etc.]: ", ))
 
-if lengthOfList == len(lst)-1:
-    print("yes")
+if lengthOfList == len(lst)+1:
+    for index, element in enumerate(lst, start=1):
+        if index != element:
+            print(index)
+            
 else: 
-    print("no")
+    print("List length is not matching!")
+
+    
