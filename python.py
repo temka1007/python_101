8d917e7ae2e11260445adbddba51a649f0bbcabd
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-10-25 15:37:13 +0900

    lab 6 output to a text file

--- python.py
+++ python.py
@@ -1,6 +1,22 @@
-with open("./test.txt", "r") as chainsaw: 
-    for line in chainsaw:
-        print(line, end=" ")
+def write_file(param):
+    content = """1 2 3 4 5 6 7 8 9
+2 4 6 8 10 12 14 16 18
+3 6 9 12 15 18 21 24 27
+4 8 12 16 20 24 28 32 36
+5 10 15 20 25 30 35 40 45
+6 12 18 24 30 36 42 48 54
+7 14 21 28 35 42 49 56 63
+8 16 24 32 40 48 56 64 72
+9 18 27 36 45 54 63 72 81"""
 
+    with open(f"{param}", "w") as table:
+        table.write(content)
 
+def read_file(param):
+    with open(f"{param}", "r") as table:
+        table_content = table.read()
+        print(table_content)
 
+filename = "multiplication_table.txt"
+write_file(filename)
+read_file(filename)
