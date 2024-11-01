61add1c21638a3ef477fa0140b9027865eefd03e
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-11-01 17:45:12 +0900

    Organize student grades using a dictionary

--- python.py
+++ python.py
@@ -1,22 +1,40 @@
-def write_file(param):
-    content = """1 2 3 4 5 6 7 8 9
-2 4 6 8 10 12 14 16 18
-3 6 9 12 15 18 21 24 27
-4 8 12 16 20 24 28 32 36
-5 10 15 20 25 30 35 40 45
-6 12 18 24 30 36 42 48 54
-7 14 21 28 35 42 49 56 63
-8 16 24 32 40 48 56 64 72
-9 18 27 36 45 54 63 72 81"""
+course_titles = ['Linear Algebra', 'Cell Biology', 'Programming']
+midterm_scores = {
+    'Alice': [62, 60, 77],
+    'Bob': [84, 88, 87],
+    'Carol': [88, 88, 97],
+    'Chuck': [92, 71, 93],
+    'Craig': [81, 89, 72],
+    'Dan': [79, 100, 97],
+    'Erin': [76, 72, 65],
+    'Eve': [69, 84, 67],
+    'Faythe': [66, 60, 70],
+    'Frank': [88, 76, 64],
+    'Grace': [66, 77, 92],
+    'Heidi': [93, 93, 82],
+    'Mallory': [77, 89, 82],
+    'Oscar': [67, 63, 67],
+    'Peggy': [70, 81, 86],
+    'Sybil': [96, 94, 62],
+    'Trent': [67, 66, 61],
+    'Trudy': [85, 74, 90],
+    'Victor': [82, 67, 94],
+    'Walter': [67, 84, 86],
+    'Wendy': [97, 92, 66]
+}
 
-    with open(f"{param}", "w") as table:
-        table.write(content)
 
-def read_file(param):
-    with open(f"{param}", "r") as table:
-        table_content = table.read()
-        print(table_content)
+def print_scores(param):
+    if param in midterm_scores.keys():
+        print(f"""{param}:
+    {course_titles[0]}: {midterm_scores[param][0]}
+    {course_titles[1]}: {midterm_scores[param][1]}
+    {course_titles[2]}: {midterm_scores[param][2]}
+    Average: {sum(midterm_scores[param])/len(midterm_scores[param])}""")
+    else:
+        print(f"There is no student named {param}")
 
-filename = "multiplication_table.txt"
-write_file(filename)
-read_file(filename)
+print_scores(input())
+
+
+    
