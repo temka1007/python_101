5e5155356b2eacf4c2171256c6d217eeaf4bb598
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-11-08 16:19:22 +0900

    Lab 6

--- calculator.py
+++ calculator.py
@@ -1,9 +1,68 @@
-def func(param1, param2):
-    lst = {char for char in param1 if char in param2}
-    return sorted(list(lst))
+attendance_data = {
+    'Math Club': {
+        'Alice': 'Present', 
+        'Bob': 'Absent', 
+        'Carol': 'Present'
+        },
+    'Science Club': {
+        'Alice': 'Absent', 
+        'Bob': 'Present', 
+        'Carol': 'Present'
+        },
+    'Book Club': {
+        'Alice': 'Present', 
+        'Carol': 'Absent', 
+        'Dan': 'Present'
+        },
+    'Music Club': {
+        'Bob': 'Present', 
+        'Dan': 'Absent', 
+        'Carol': 'Present'
+        }
+}
 
-param = input().split(' ')  
 
-output = func(param[0], param[1])
+def attendance_report(student_name):
+    present = 0
+    taken = 4
+    try:
+        math = attendance_data["Math Club"][student_name]
+        if math == "Present": present += 1
+    except:
+        math = "Didn't take the this class!"
+        taken -= 1
 
-print(output)
+    try:
+        science = attendance_data["Science Club"][student_name]
+        if science == "Present": present += 1
+        
+    except:
+        science = "Didn't take the this class!"
+        taken -= 1
+
+    try:
+        book = attendance_data["Book Club"][student_name]
+        if book == "Present": present += 1
+    except:
+        book = "Didn't take the this class!"
+        taken -= 1
+
+    try:
+        music = attendance_data["Music Club"][student_name]
+        if music == "Present": present += 1
+    except:
+        music = "Didn't take the this class!"
+        taken -= 1
+
+    if student_name in attendance_data['Book Club'].keys():
+        print(f"""{student_name}:
+    Math Club: {math}
+    Science Club: {science}
+    Book Club: {book}
+    Music Club: {music}
+    Total Events Attended: {present}
+    Attendance Percentage: {round((present/taken)*100, 1)}%""")
+    else:
+        print(f"There is no student named {student_name}.")
+
+attendance_report(input())
