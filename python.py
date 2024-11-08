a7e3594d5defb3658d3e93bdeac759c3b19024ee
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-11-08 18:34:06 +0900

    Organize and Compare Language

--- python.py
+++ python.py
@@ -1,40 +1,33 @@
-course_titles = ['Linear Algebra', 'Cell Biology', 'Programming']
-midterm_scores = {
-    'Alice': [62, 60, 77],
-    'Bob': [84, 88, 87],
-    'Carol': [88, 88, 97],
-    'Chuck': [92, 71, 93],
-    'Craig': [81, 89, 72],
-    'Dan': [79, 100, 97],
-    'Erin': [76, 72, 65],
-    'Eve': [69, 84, 67],
-    'Faythe': [66, 60, 70],
-    'Frank': [88, 76, 64],
-    'Grace': [66, 77, 92],
-    'Heidi': [93, 93, 82],
-    'Mallory': [77, 89, 82],
-    'Oscar': [67, 63, 67],
-    'Peggy': [70, 81, 86],
-    'Sybil': [96, 94, 62],
-    'Trent': [67, 66, 61],
-    'Trudy': [85, 74, 90],
-    'Victor': [82, 67, 94],
-    'Walter': [67, 84, 86],
-    'Wendy': [97, 92, 66]
+language_skills: dict = {
+    'Alice': {'English', 'Spanish', 'German'},
+    'Bob': {'English', 'French'},
+    'Carol': {'Spanish', 'German', 'Japanese'},
+    'Dan': {'English', 'Spanish', 'Korean'},
+    'Eve': {'German', 'Korean'}
 }
 
+def compare_languages(student1, student2):
+    if student1 not in language_skills.keys():
+        print(f"There is no student named {student1}.")
+        exit()
 
-def print_scores(param):
-    if param in midterm_scores.keys():
-        print(f"""{param}:
-    {course_titles[0]}: {midterm_scores[param][0]}
-    {course_titles[1]}: {midterm_scores[param][1]}
-    {course_titles[2]}: {midterm_scores[param][2]}
-    Average: {sum(midterm_scores[param])/len(midterm_scores[param])}""")
-    else:
-        print(f"There is no student named {param}")
-
-print_scores(input())
+    if student2 not in language_skills.keys():
+        print(f"There is no student named {student2}.")
+        exit()
 
 
-    
+    firstStudent = language_skills[student1]
+    secondStudent = language_skills[student2]
+
+    bothKnows: set = firstStudent & secondStudent
+    uniqueToStudent1: set = firstStudent.difference(secondStudent)
+    uniqueToStudent2: set = secondStudent.difference(firstStudent)
+
+    print(f'''{student1} knows: {firstStudent}
+{student2} knows: {secondStudent}
+Languages known by both {student1} and {student2}: {bothKnows}
+Languages unique to {student1}: {uniqueToStudent1}
+Languages unique to {student2}: {uniqueToStudent2}''')
+
+
+compare_languages(input("Enter first student: "), input("Enter second student: "))
