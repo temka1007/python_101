19c19cac830f8da12fdbd2f4aa8c2fea8164153b
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-11-12 22:44:11 +0900

    spin words

--- python.py
+++ python.py
@@ -1,33 +1,14 @@
-language_skills: dict = {
-    'Alice': {'English', 'Spanish', 'German'},
-    'Bob': {'English', 'French'},
-    'Carol': {'Spanish', 'German', 'Japanese'},
-    'Dan': {'English', 'Spanish', 'Korean'},
-    'Eve': {'German', 'Korean'}
-}
-
-def compare_languages(student1, student2):
-    if student1 not in language_skills.keys():
-        print(f"There is no student named {student1}.")
-        exit()
-
-    if student2 not in language_skills.keys():
-        print(f"There is no student named {student2}.")
-        exit()
+def spin_words(sentence):
+    lst = sentence.split(" ")
+    count = 0
+    for word in lst:
+        if len(word) >= 5:
+            print(True)
+            lst[count] = lst[count][::-1]
+        count += 1
+    return " ".join(lst)
 
 
-    firstStudent = language_skills[student1]
-    secondStudent = language_skills[student2]
+test = "Hey fellow warriors"
 
-    bothKnows: set = firstStudent & secondStudent
-    uniqueToStudent1: set = firstStudent.difference(secondStudent)
-    uniqueToStudent2: set = secondStudent.difference(firstStudent)
-
-    print(f'''{student1} knows: {firstStudent}
-{student2} knows: {secondStudent}
-Languages known by both {student1} and {student2}: {bothKnows}
-Languages unique to {student1}: {uniqueToStudent1}
-Languages unique to {student2}: {uniqueToStudent2}''')
-
-
-compare_languages(input("Enter first student: "), input("Enter second student: "))
+print(spin_words(test))
