5b692f1d5dcbf124d090bb930188cd072648d6a4
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-12-01 13:22:11 +0900

    bit counting

--- test_center.py
+++ test_center.py
@@ -1,18 +1,13 @@
-def binary_search_recursive(arr, target, left, right):
-    if left > right:
-        return -1
+def count_bits(n):
+    remainder = n
+    total = []
+    while remainder > 0:
+        total.insert(0, remainder % 2)
+        remainder = remainder // 2
+        print(remainder)
 
-    midpoint_index = (left + right) // 2
-
-    if arr[midpoint_index] == target:
-        return midpoint_index
-    elif arr[midpoint_index] < target:
-        return binary_search_recursive(arr, target, midpoint_index + 1, right)
-    else:
-        return binary_search_recursive(arr, target, left, midpoint_index - 1)
+    print(total)
+    return sum(total)
 
 
-arr = [2, 4, 6, 8, 10, 12, 14, 16]
-target = 14
-result = binary_search_recursive(arr, target, 0, len(arr) - 1)
-print(f"Index of {target}: {result}")
+print(count_bits(16))
